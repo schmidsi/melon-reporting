38915f7e705b716a6b143c6450a2820032593fc9
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-04-03 16:43:42 +0200

    change auditing contract to directly process rsv values

--- packages/contracts/Auditing.sol
+++ packages/contracts/Auditing.sol
@@ -6,7 +6,9 @@ contract Auditing {
     struct Audit {
         address auditor;
         bytes32 dataHash;
-        bytes32 signature;
+        bytes32 r;
+        bytes32 s;
+        uint8 v;
         // absolute unix timestamp (seconds since 1970-01-01)
         uint timestamp;
     }
@@ -20,11 +22,11 @@ contract Auditing {
 
     /// Create a new audit on a fund specified with `fundAddress`,
     /// the hashed data in `dataHash` and a `signature`.
-    function audit(address _fundAddress, bytes32 _dataHash, bytes32 _signature) 
+    function audit(address _fundAddress, bytes32 _dataHash, bytes32 _r, bytes32 _s, uint8 _v) 
             public {
-        require(checkSignature(_dataHash, _signature, msg.sender));
+        require(checkSignature(_dataHash, _r, _s, _v, msg.sender));
 
-        Audit memory newAudit = Audit(msg.sender, _dataHash, _signature, now);
+        Audit memory newAudit = Audit(msg.sender, _dataHash, _r, _s, _v, now);
         fundAudits[_fundAddress].push(newAudit);
 
         uint index = fundAudits[_fundAddress].length - 1;
@@ -34,11 +36,13 @@ contract Auditing {
     /// Get the last audit stored for a specific `fundAddress`.
     function getLastAudit(address _fundAddress) 
             public view 
-            returns (address auditor, bytes32 dataHash, bytes32 signature, uint timestamp) {
+            returns (address auditor, bytes32 dataHash, bytes32 r, bytes32 s, uint8 v, uint timestamp) {
         Audit memory lastAudit = fundAudits[_fundAddress][fundAudits[_fundAddress].length - 1];
         auditor = lastAudit.auditor;
         dataHash = lastAudit.dataHash;
-        signature = lastAudit.signature;
+        r = lastAudit.r;
+        s = lastAudit.s;
+        v = lastAudit.v;
         timestamp = lastAudit.timestamp;
     }
 
@@ -50,13 +54,13 @@ contract Auditing {
     }
 
     /// Returns true if the audit is on the blockchain (present in fundAudits)
-    function verifyAudit(address _fundAddress, bytes32 _dataHash, bytes32 _signature, address _auditor)
+    function verifyAudit(address _fundAddress, bytes32 _dataHash, bytes32 _r, bytes32 _s, uint8 _v, address _auditor)
             public view
             returns (bool found) {
         Audit[] memory audits = fundAudits[_fundAddress];
         for (uint i = 0; i < audits.length; i++) {
             Audit memory tempAudit = audits[i];
-            if (tempAudit.auditor == _auditor && tempAudit.dataHash == _dataHash && tempAudit.signature == _signature) {
+            if (tempAudit.auditor == _auditor && tempAudit.dataHash == _dataHash && tempAudit.r == _r && tempAudit.s == _s && tempAudit.v == _v) {
                 return true;
             }
         }
@@ -64,43 +68,14 @@ contract Auditing {
     }
 
     /// Check if the signature and datahash corresponds with the sender
-    function checkSignature(bytes32 _dataHash, bytes32 _signature, address _sender)
-            private
+    function checkSignature(bytes32 _dataHash, bytes32 _r, bytes32 _s, uint8 _v, address _sender)
+            public 
             pure
             returns (bool signatureVerified) {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        bytes memory sig = substring(_signature, 2, 132);
-
-        assembly {
-            r := mload(add(sig, 32))
-            s := mload(add(sig, 64))
-            v := byte(0, mload(add(sig, 96)))
-        }
-
-        if (v < 27) {
-            v += 27;
-        }
-
-        address recoveredAddress = ecrecover(_dataHash, v, r, s);
+        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+        bytes32 prefixedHash = keccak256(prefix, _dataHash);
+        address recoveredAddress = ecrecover(prefixedHash, _v, _r, _s);
         return _sender == recoveredAddress;
-    }
-
-    /// Helper function for checkSignature()
-    function substring(bytes32 _str, uint _startIndex, uint _endIndex) 
-            public pure 
-            returns (bytes) {
-        require(_startIndex <= _endIndex);
-        require(_startIndex >= 0);
-        require(_endIndex <= _str.length);
-
-        bytes memory result = new bytes(_endIndex - _startIndex);
-        for (uint i = _startIndex; i < _endIndex; i++) {
-            result[i - _startIndex] = _str[i];
-        }
-        return bytes(result);
     }
 
 }
