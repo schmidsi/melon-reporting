48ae48f0f9c694ebd8a5dbd48868b8fca222d487
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-04-13 11:42:07 +0200

    reimplement auditing contract according to spec

--- packages/contracts/Auditing.sol
+++ packages/contracts/Auditing.sol
@@ -6,76 +6,71 @@ contract Auditing {
     struct Audit {
         address auditor;
         bytes32 dataHash;
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
         // absolute unix timestamp (seconds since 1970-01-01)
         uint timestamp;
     }
 
     // events
-    event AuditReported(address _fundAddress, uint _index);
+    event Added(address _fundAddress, uint256 _index);
 
     // for this mapping, a getter is created 
     // where we can retrieve audits by its fundAddress and position
     mapping(address => Audit[]) public fundAudits;
 
-    /// Create a new audit on a fund specified with `fundAddress`,
-    /// the hashed data in `dataHash` and a `signature`.
-    function audit(address _fundAddress, bytes32 _dataHash, bytes32 _r, bytes32 _s, uint8 _v) 
-            public {
-        require(checkSignature(_dataHash, _r, _s, _v, msg.sender));
+    // TODO auditor approval through ctor
+    mapping(address => address[]) public approvedAuditors;
 
-        Audit memory newAudit = Audit(msg.sender, _dataHash, _r, _s, _v, now);
+    /// Creates a new audit on a fund specified with `fundAddress`,
+    /// the hashed data in `dataHash` and a `signature`.
+    function add(address _fundAddress, bytes32 _dataHash) 
+            public {
+        // TODO check if the sender is an approved auditor with "require"
+        Audit memory newAudit = Audit(msg.sender, _dataHash, now);
         fundAudits[_fundAddress].push(newAudit);
 
         uint index = fundAudits[_fundAddress].length - 1;
-        emit AuditReported(_fundAddress, index);
+        emit Added(_fundAddress, index);
     }
 
-    /// Get the last audit stored for a specific `fundAddress`.
-    function getLastAudit(address _fundAddress) 
+    /// Verifies that the provided data is mapped to an existing audit
+    function verify(address _fundAddress, address _auditor, bytes32 _dataHash) 
             public view 
-            returns (address auditor, bytes32 dataHash, bytes32 r, bytes32 s, uint8 v, uint timestamp) {
-        Audit memory lastAudit = fundAudits[_fundAddress][fundAudits[_fundAddress].length - 1];
-        auditor = lastAudit.auditor;
-        dataHash = lastAudit.dataHash;
-        r = lastAudit.r;
-        s = lastAudit.s;
-        v = lastAudit.v;
-        timestamp = lastAudit.timestamp;
-    }
-
-    /// Get the amount of audits for a specific `fundAddress`
-    function getAuditCount(address _fundAddress)
-            public view
-            returns (uint count) {
-        count = fundAudits[_fundAddress].length;
-    }
-
-    /// Returns true if the audit is on the blockchain (present in fundAudits)
-    function verifyAudit(address _fundAddress, bytes32 _dataHash, bytes32 _r, bytes32 _s, uint8 _v, address _auditor)
-            public view
-            returns (bool found) {
+            returns (bool auditIsVerified) {
         Audit[] memory audits = fundAudits[_fundAddress];
         for (uint i = 0; i < audits.length; i++) {
-            Audit memory tempAudit = audits[i];
-            if (tempAudit.auditor == _auditor && tempAudit.dataHash == _dataHash && tempAudit.r == _r && tempAudit.s == _s && tempAudit.v == _v) {
+            Audit memory audit = audits[i];
+            if (audit.auditor == _auditor && audit.dataHash == _dataHash) {
+                // audit is verified
                 return true;
             }
         }
-        return false; // audit not found
+        // audit is not verified
+        return false;
     }
 
-    /// Check if the signature and datahash corresponds with the sender
-    function checkSignature(bytes32 _dataHash, bytes32 _r, bytes32 _s, uint8 _v, address _sender)
-            public 
-            pure
-            returns (bool signatureVerified) {
-        bytes memory prefix = "\x19Ethereum Signed Message:\n32";
-        bytes32 prefixedHash = keccak256(prefix, _dataHash);
-        address recoveredAddress = ecrecover(prefixedHash, _v, _r, _s);
-        return _sender == recoveredAddress;
+    /// Returns the last index of a specific fund
+    function getLastIndex(address _fundAddress)
+            public view
+            returns (uint256 index) {
+        Audit[] memory audits = fundAudits[_fundAddress];
+        if (audits.length == 0) {
+            // return maxvalue when no audits for a fund are stored
+            return 2**256 - 1;
+        } else {
+            return audits.length - 1;
+        }
+    }
+
+    /// Returns the requested audit data
+    function getByIndex(address _fundAddress, uint256 _index)
+            public view
+            returns (bytes32 dataHash, address auditor, uint256 timestamp) {
+        require(_index < fundAudits[_fundAddress].length); // index must be smaller than array length
+
+        Audit memory audit = fundAudits[_fundAddress][_index];
+        dataHash = audit.dataHash;
+        auditor = audit.auditor;
+        timestamp = audit.timestamp;
     }
 
 }
