5227d6119ccffae65a5be5880e7c0d857a7d72a0
Simon Emanuel Schmid <simon@schmid.io>
2018-04-13 11:51:19 +0200

    Deploy book with now

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 npm run gitbook build
-git branch -D gh-pages
-git push -d origin gh-pages
-git checkout --orphan gh-pages
-git add -f _book
-git commit -m "Deploy"
-git subtree push --prefix _book origin gh-pages
-git checkout master
-git branch -D gh-pages
+cd _book
+now
+now alias
