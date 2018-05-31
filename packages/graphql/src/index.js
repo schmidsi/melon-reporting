54fe67bd62ec835dd3cc00462420a6604e3297de
Simon Emanuel Schmid <simon@schmid.io>
2018-05-31 17:39:08 +0200

    Example blockchain watcher with infura websockets

--- packages/graphql/src/index.js
+++ packages/graphql/src/index.js
@@ -1,83 +1,14 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
+const Web3 = require('web3');
+const ERC20ABI = require('./abi/ERC20Interface.json');
+
+const getEvent = (ABI, event) =>
+  ABI.find(e => e.type === 'event' && e.name === event);
 
 const resolvers = {
-  Query: {
-    posts: (_, args, context, info) => {
-      return context.prisma.query.posts(
-        {
-          where: {
-            OR: [
-              { title_contains: args.searchString },
-              { content_contains: args.searchString },
-            ],
-          },
-        },
-        info,
-      );
-    },
-    user: (_, args, context, info) => {
-      return context.prisma.query.user(
-        {
-          where: {
-            id: args.id,
-          },
-        },
-        info,
-      );
-    },
-  },
-  Mutation: {
-    createDraft: (_, args, context, info) => {
-      return context.prisma.mutation.createPost(
-        {
-          data: {
-            title: args.title,
-            content: args.title,
-            author: {
-              connect: {
-                id: args.authorId,
-              },
-            },
-          },
-        },
-        info,
-      );
-    },
-    publish: (_, args, context, info) => {
-      return context.prisma.mutation.updatePost(
-        {
-          where: {
-            id: args.id,
-          },
-          data: {
-            published: true,
-          },
-        },
-        info,
-      );
-    },
-    deletePost: (_, args, context, info) => {
-      return context.prisma.mutation.deletePost(
-        {
-          where: {
-            id: args.id,
-          },
-        },
-        info,
-      );
-    },
-    signup: (_, args, context, info) => {
-      return context.prisma.mutation.createUser(
-        {
-          data: {
-            name: args.name,
-          },
-        },
-        info,
-      );
-    },
-  },
+  // Query: {},
+  // Mutation: {},
 };
 
 const server = new GraphQLServer({
@@ -92,6 +23,40 @@ const server = new GraphQLServer({
   }),
 });
 
-server.start(() =>
-  console.log(`GraphQL server is running on http://localhost:4000`),
-);
+server.start(async () => {
+  console.log(`GraphQL server is running on http://localhost:4000`);
+
+  const web3 = new Web3('wss://mainnet.infura.io/ws');
+
+  const transferEvent = getEvent(ERC20ABI, 'Transfer');
+  const transferSignature = web3.eth.abi.encodeEventSignature(transferEvent);
+
+  console.log(transferEvent, transferSignature);
+
+  const subscription = web3.eth.subscribe(
+    'logs',
+    {
+      address: '0x86fa049857e0209aa7d9e616f7eb3b3b78ecfdb0',
+      topics: [transferSignature],
+    },
+    (a, b, c) => {
+      // console.log('======== callback', a, b, c);
+    },
+  );
+
+  subscription.on('data', (log, b) => {
+    const decoded = web3.eth.abi.decodeLog(
+      transferEvent.inputs,
+      log.data,
+      log.topics.slice(1),
+    );
+
+    console.log('======== data', log, decoded);
+  });
+  subscription.on('changed', (log, b) => {
+    console.log('======== changed', log, b);
+  });
+  subscription.on('error', (log, b) => {
+    console.log('======== error', log, b);
+  });
+});
