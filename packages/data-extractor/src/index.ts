2fa4373666830ea5a6c8184ed2f7b57acadf8d11
Simon Emanuel Schmid <simon@schmid.io>
2018-06-16 16:36:18 +0200

    Return ranking if no fundAddress

--- packages/data-extractor/src/index.ts
+++ packages/data-extractor/src/index.ts
@@ -1,40 +1,15 @@
 import {
-  getParityProvider,
   getConfig,
-  performCalculations,
   getFundInformations,
   getHoldingsAndPrices,
+  getParityProvider,
+  getRanking,
+  performCalculations,
 } from '@melonproject/melon.js';
 import * as match from 'micro-match';
 import * as Joi from 'joi';
 
-module.exports = async req => {
-  console.log(req.url);
-
-  const start = Date.now();
-
-  const paramsSchema = Joi.object().keys({
-    fundAddress: Joi.string().regex(/^0x(\d|[A-F]|[a-f]){40}$/s),
-    timeSpanStart: Joi.number()
-      .integer()
-      .min(0)
-      .max(Date.now() / 1000),
-    timeSpanEnd: Joi.number()
-      .integer()
-      .min(Joi.ref('timeSpanStart'))
-      .max(Date.now() / 1000),
-  });
-
-  const params = match('/:fundAddress?/:timeSpanStart?/:timeSpanEnd?', req.url);
-
-  const { error, value } = Joi.validate(params, paramsSchema);
-
-  console.log(params, error, value);
-
-  if (error) return error;
-
-  const fundAddress = '0x7610BC396863eEcDae03440756D992c82821F805';
-
+const dataExtractor = async (fundAddress, timeSpanStart, timeSpanEnd) => {
   const environment = await getParityProvider('https://kovan.melonport.com');
   // 'https://kovan.melonport.com' ~ 605ms
   // 'https://kovan.infura.io/l8MnVFI1fXB7R6wyR22C' ~ 2000ms
@@ -60,7 +35,44 @@ module.exports = async req => {
     })),
   );
 
+  return { config, informations, calculations, holdings };
+};
+
+const ranking = async () => {
+  const environment = await getParityProvider('https://kovan.melonport.com');
+  return getRanking(environment);
+};
+
+module.exports = async req => {
+  const start = Date.now();
+
+  const paramsSchema = Joi.object().keys({
+    fundAddress: Joi.string().regex(/^0x(\d|[A-F]|[a-f]){40}$/s),
+    timeSpanStart: Joi.number()
+      .integer()
+      .min(0)
+      .max(Date.now() / 1000),
+    timeSpanEnd: Joi.number()
+      .integer()
+      .min(Joi.ref('timeSpanStart'))
+      .max(Date.now() / 1000),
+  });
+
+  const params = match('/:fundAddress?/:timeSpanStart?/:timeSpanEnd?', req.url);
+
+  const { error, value } = Joi.validate(params, paramsSchema);
+
+  console.log(params, error, value);
+
+  if (error) return error;
+
+  const { fundAddress, timeSpanStart, timeSpanEnd } = value;
+
+  // const fundAddress = '0x7610BC396863eEcDae03440756D992c82821F805';
+  const data = fundAddress
+    ? await dataExtractor(fundAddress, timeSpanStart, timeSpanEnd)
+    : await ranking();
   const end = Date.now();
   const duration = end - start;
-  return { config, informations, calculations, holdings, duration };
+  return { data, duration };
 };
