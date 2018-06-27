037fc80600064895ed95c9c72bdbcb2e8803cf94
Simon Emanuel Schmid <simon@schmid.io>
2018-06-27 15:19:29 +0200

    First step styled-jsx

--- packages/web-interface/src/pages/report.js
+++ packages/web-interface/src/pages/report.js
@@ -4,6 +4,11 @@ import dataExtractor from '@melonproject/data-extractor/dataExtractor';
 
 const Report = ({ data, debug }) => (
   <div>
+    <style global jsx>{`
+      body {
+        background-color: rgb(255, 253, 244);
+      }
+    `}</style>
     <h1>Report</h1>
     <pre>{JSON.stringify(data, null, 4)}</pre>
   </div>
