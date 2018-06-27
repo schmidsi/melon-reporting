4a5acef65561703c4f084a4567c467a1c0433698
Simon Emanuel Schmid <simon@schmid.io>
2018-06-27 15:36:47 +0200

    Extract styles in own file

--- packages/web-interface/src/pages/report.js
+++ packages/web-interface/src/pages/report.js
@@ -2,13 +2,13 @@ import Link from 'next/link';
 
 import dataExtractor from '@melonproject/data-extractor/dataExtractor';
 
+import styles, { button } from '../styles/app';
+
 const Report = ({ data, debug }) => (
   <div>
-    <style global jsx>{`
-      body {
-        background-color: rgb(255, 253, 244);
-      }
-    `}</style>
+    <style jsx global>
+      {styles}
+    </style>
     <h1>Report</h1>
     <pre>{JSON.stringify(data, null, 4)}</pre>
   </div>
---
+++ packages/web-interface/src/styles/app.js
@@ -0,0 +1,13 @@
+import css from 'styled-jsx/css';
+
+export default css`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: rgb(255, 253, 244);
+    font-family: 'Source Serif Pro', serif;
+    font-size: 16px;
+  }
+`;
