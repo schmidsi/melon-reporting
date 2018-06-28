a01f7940e1517ab11baf118ca447e9fa2c5a618b
Simon Emanuel Schmid <simon@schmid.io>
2018-06-28 18:22:23 +0200

    Change from child function to bare object

--- packages/components/src/blocks/DescriptionList/README.md
+++ packages/components/src/blocks/DescriptionList/README.md
@@ -4,10 +4,10 @@ Example:
 
 ```js
 <DescriptionList size={7} detailsAlign="right">
-  {() => ({
+  {{
     Profit: <span style={{ color: 'green' }}>+50%</span>,
     Address: '0xdeadbeef',
     Category: 'Manager Traded Fund',
-  })}
+  }}
 </DescriptionList>
 ```
--- packages/components/src/blocks/DescriptionList/index.jsx
+++ packages/components/src/blocks/DescriptionList/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DescriptionList = ({ children }) => (
   <dl>
-    {Object.entries(children()).map(([key, value]) => (
+    {Object.entries(children).map(([key, value]) => (
       <React.Fragment key={key}>
         <dt>{key}</dt>
         <dd>{value}</dd>
