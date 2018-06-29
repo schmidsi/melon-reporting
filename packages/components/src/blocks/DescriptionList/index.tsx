31dda5495c765c550eacc4bd236c095274fa9dc4
Simon Emanuel Schmid <simon@schmid.io>
2018-06-29 15:05:24 +0200

    Import css with styled-jsx & postcss

--- packages/components/src/blocks/DescriptionList/index.tsx
+++ packages/components/src/blocks/DescriptionList/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import styles from './styles.css';
+
 const DescriptionList = ({ children }) => (
   <dl>
+    <style jsx>{styles}</style>
     {Object.entries(children).map(([key, value]) => (
       <React.Fragment key={key}>
         <dt>{key}</dt>
--- packages/components/src/blocks/Test/README.md
+++
@@ -1,5 +0,0 @@
-Test example:
-
-```js
-<Test />
-```
