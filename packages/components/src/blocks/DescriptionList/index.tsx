28b8f4a3f2fee7a294f76dc3b75c0dd909897d96
Simon Emanuel Schmid <simon@schmid.io>
2018-06-30 14:49:06 +0200

    Make description list a table

--- packages/components/src/blocks/DescriptionList/index.tsx
+++ packages/components/src/blocks/DescriptionList/index.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import styles from './styles.css';
 
 const DescriptionList = ({ children }) => (
-  <dl>
-    <style jsx>{styles}</style>
-    {Object.entries(children).map(([key, value]) => (
-      <React.Fragment key={key}>
-        <dt>{key}</dt>
-        <dd>{value}</dd>
-      </React.Fragment>
-    ))}
-  </dl>
+  <div>
+    <table className="description-list">
+      <style jsx>{styles}</style>
+      {Object.entries(children).map(([key, value]) => (
+        <tr>
+          <th>{key}:</th>
+          <td>{value}</td>
+        </tr>
+      ))}
+    </table>
+  </div>
 );
 
 export default DescriptionList;
--- packages/components/src/blocks/DescriptionList/story.tsx
+++ packages/components/src/blocks/DescriptionList/story.tsx
@@ -3,12 +3,17 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import DescriptionList from './';
 
+import global from '../../global.css';
+
 storiesOf(DescriptionList.name, module).add('with text', () => (
-  <DescriptionList size={7} detailsAlign="right">
-    {{
-      Profit: <span style={{ color: 'green' }}>+50%</span>,
-      Address: '0xdeadbeef',
-      Category: 'Manager Traded Fund',
-    }}
-  </DescriptionList>
+  <div>
+    <style jsx>{global}</style>
+    <DescriptionList size={7} detailsAlign="right">
+      {{
+        Profit: <span style={{ color: 'green' }}>+50%</span>,
+        Address: '0xdeadbeef',
+        Category: 'Manager Traded Fund',
+      }}
+    </DescriptionList>
+  </div>
 ));
