31ad6fa89c27efb3d09b6280e17dee2cba1716d2
Simon Emanuel Schmid <simon@schmid.io>
2018-06-30 15:45:13 +0200

    Add info addon

--- packages/components/config/config.js
+++ packages/components/config/config.js
@@ -1,7 +1,15 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
 
 const stories = require.context('../src', true, /\/story\.(tsx?)$/);
 
 configure(() => {
   return stories.keys().forEach(stories);
 }, module);
+
+setOptions({
+  hierarchySeparator: /\/|\./, // matches a . or /
+  hierarchyRootSeparator: /\|/, //matches a |
+  name: 'Melon Reporting',
+  url: '#',
+});
--- packages/components/src/blocks/DescriptionList/index.tsx
+++ packages/components/src/blocks/DescriptionList/index.tsx
@@ -6,12 +6,14 @@ const DescriptionList = ({ children }) => (
   <div>
     <table className="description-list">
       <style jsx>{styles}</style>
-      {Object.entries(children).map(([key, value]) => (
-        <tr>
-          <th>{key}:</th>
-          <td>{value}</td>
-        </tr>
-      ))}
+      <tbody>
+        {Object.entries(children).map(([key, value]) => (
+          <tr key={key}>
+            <th>{key}:</th>
+            <td>{value}</td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   </div>
 );
