c0af521a097f09cec46496140bca5821ee03f096
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-07-04 14:18:33 +0200

    Hash data object

--- src/prototype/src/report/FundReportHasher.js
+++ src/prototype/src/report/FundReportHasher.js
@@ -1,7 +1,6 @@
 import { md as _md } from "node-forge";
 
 export function hashReport(report) {
-  // maybe use MD5 which fits into single bytes32 solidity value
   var md = _md.sha256.create();
   md.update(report);
   return "0x" + md.digest().toHex();
