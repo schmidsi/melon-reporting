0614c10e51bf97805925566289f1364f83485213
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-07-04 10:12:56 +0200

    Fix ranking path

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import * as R from 'ramda';
 
-import getRanking from '@melonproject/data-extractor/ranking';
+import getRanking from '../api/ranking';
 
 const Browse = ({ ranking }) => (
   <div>
     <h1>Browse</h1>
-    {ranking.map(rank => <div>{rank}</div>)}
+    {ranking.map(rank => <div>{rank.address}</div>)}
     {R.isEmpty(ranking) && <p>No funds on this version or price feed down</p>}
   </div>
 );
