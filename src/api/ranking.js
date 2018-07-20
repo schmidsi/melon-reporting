cb480defe9ee4d7831a94d0ed50e62bce48e1ec2
Benjamin Zumbrunn <benzumbrunn@gmail.com>
2018-07-20 11:29:41 +0200

    Update to newer melon.js, fix getRanking

--- src/api/ranking.js
+++ src/api/ranking.js
@@ -2,7 +2,7 @@ import { getParityProvider, getRanking } from '@melonproject/melon.js';
 
 const ranking = async () => {
   const environment = await getParityProvider();
-  return getRanking(environment);
+  return getRanking({ ...environment, track: "kovan-demo" });
 };
 
 export default ranking;
