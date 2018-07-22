9428a6a6b31b3d44d1954e89df31a2cb1e6b16c1
Simon Emanuel Schmid <simon@schmid.io>
2018-07-22 19:18:54 +0200

    Data loading pattern with recompose

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
 import Report from './pages/Report';
+import Browse from './pages/Browse';
 
 const main = async () => {
-  const { data, calculations } = await Report.getInitialProps({
-    query: {
-      fundAddress: '0xbada55',
-      timeSpanStart: 1514761200,
-      timeSpanEnd: 1522447200,
-    },
-  });
+  // const { data, calculations } = await Report.getInitialProps({
+  //   query: {
+  //     fundAddress: '0xbada55',
+  //     timeSpanStart: 1514761200,
+  //     timeSpanEnd: 1522447200,
+  //   },
+  // });
 
   ReactDOM.render(
-    <Report data={data} calculations={calculations} />,
+    <BrowserRouter>
+      <React.Fragment>
+        <Redirect exact from="/" to="/browse" />
+        <Route path="/browse" component={Browse} />
+        {/* <Report data={data} calculations={calculations} /> */}
+      </React.Fragment>
+    </BrowserRouter>,
     document.getElementById('root'),
   );
 };
--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,18 +1,27 @@
 import * as R from 'ramda';
-import { Link } from '../routes';
+import React from 'react';
+import {
+  branch,
+  compose,
+  lifecycle,
+  withStateHandlers,
+  renderComponent,
+} from 'recompose';
 
 import getRanking from '../api/ranking';
 
-const Browse = ({ ranking }) => (
+const Spinner = () => <div>Loading...</div>;
+
+const Browse = ({ ranking = [] }) => (
   <div>
     <h1>Browse</h1>
     {ranking.map(fund => (
       <div key={fund.address}>
-        <Link route="defaultTimespan" params={{ fundAddress: fund.address }}>
-          <a>
-            {fund.name} - {fund.address}
-          </a>
-        </Link>
+        {/* <Link route="defaultTimespan" params={{ fundAddress: fund.address }}> */}
+        <a>
+          {fund.name} - {fund.address}
+        </a>
+        {/* </Link> */}
       </div>
     ))}
     {R.isEmpty(ranking) && <p>No funds on this version or price feed down</p>}
@@ -24,4 +33,24 @@ Browse.getInitialProps = async () => {
   return { ranking };
 };
 
-export default Browse;
+const enhance = compose(
+  withStateHandlers(
+    {
+      loading: true,
+      ranking: [],
+    },
+    {
+      setRanking: () => ranking => ({ ranking, loading: false }),
+    },
+  ),
+  lifecycle({
+    async componentDidMount() {
+      const ranking = await getRanking();
+      this.props.setRanking(ranking);
+      console.log(ranking, this.props);
+    },
+  }),
+  branch(({ loading }) => loading, renderComponent(Spinner)),
+);
+
+export default enhance(Browse);
