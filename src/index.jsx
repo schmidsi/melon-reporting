3a5c0104e60301ca3b6d9f862d1a3f3bd651dfdd
Simon Emanuel Schmid <simon@schmid.io>
2018-07-22 16:02:57 +0200

    Make basic report work with parcel

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Report from './pages/Report';
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
-}
+const main = async () => {
+  const { data, calculations } = await Report.getInitialProps({
+    query: {
+      fundAddress: '0xbada55',
+      timeSpanStart: 1514761200,
+      timeSpanEnd: 1522447200,
+    },
+  });
 
-var mountNode = document.getElementById('app');
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+  console.log({ data, calculations });
+
+  ReactDOM.render(
+    <Report data={data} calculations={calculations} />,
+    document.getElementById('root'),
+  );
+};
+
+main();
--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReportTemplate from '../components/templates/Report';
 
 import reportDataGenerator from '../api/reportDataGenerator';
