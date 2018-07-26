bdc0ec3cea8288165f95b79aeb4d849ac47bcb7e
Simon Emanuel Schmid <simon@schmid.io>
2018-07-26 11:57:54 +0200

    Refactor and add fund row to tokenCorrelation

--- src/components/templates/Holdings/story.jsx
+++ src/components/templates/Holdings/story.jsx
@@ -8,6 +8,45 @@ import storyCss from './story.css';
 
 import Holdings from './';
 
+// (i, j, symbol)
+const randomCorrelation = R.cond([
+  [R.lt, (i, j, symbol) => ({ [symbol]: '' })],
+  [R.equals, (i, j, symbol) => ({ [symbol]: '1' })],
+  [
+    R.T,
+    (i, j, symbol) => ({
+      [symbol]: (Math.random() * 2 - 1).toFixed(2),
+    }),
+  ],
+]);
+
+const tokenCorrelation = [
+  ...exampleData.holdings,
+  { token: { symbol: 'Fund' } },
+].map((rowHolding, rowIndex) =>
+  R.mergeAll(
+    exampleData.holdings.map((colHolding, colIndex) =>
+      randomCorrelation(rowIndex, colIndex, colHolding.token.symbol),
+    ),
+  ),
+);
+
+// Nice to have: Refactor with xprod: R.splitEvery(l2.length, R.xprod(l1, l2))
+// console.log(
+//   R.splitEvery(
+//     exampleData.holdings.length,
+//     R.xprod(
+//       exampleData.holdings.map((h, i) => ({ i })),
+//       exampleData.holdings.map((h, j) => ({
+//         j,
+//         symbol: h.token.symbol,
+//       })),
+//     )
+//       .map(R.mergeAll)
+//       .map(({ i, j, symbol }) => randomCorrelation(i, j, symbol)),
+//   ),
+// );
+
 storiesOf('Templates|Holdings', module).add('default', () => (
   <div>
     <div className={storyCss.background} />
@@ -20,32 +59,7 @@ storiesOf('Templates|Holdings', module).add('default', () => (
           transactionFees: 83.214,
           volatility: 19.5,
           profit: 5.23,
-          // Nice to have: Refactor with xprod: R.splitEvery(l2.length, R.xprod(l1, l2))
-          tokenCorrelation: exampleData.holdings.map((rowHolding, rowIndex) =>
-            R.mergeAll(
-              exampleData.holdings.map((colHolding, colIndex) =>
-                R.cond([
-                  [
-                    () => colIndex > rowIndex,
-                    () => ({ [colHolding.token.symbol]: '' }),
-                  ],
-                  [
-                    () => colIndex === rowIndex,
-                    () => ({ [colHolding.token.symbol]: '-' }),
-                  ],
-                  [
-                    () => true,
-                    () => ({
-                      [colHolding.token.symbol]: (
-                        Math.random() * 2 -
-                        1
-                      ).toFixed(2),
-                    }),
-                  ],
-                ])(),
-              ),
-            ),
-          ),
+          tokenCorrelation,
         }}
       />
     </div>
