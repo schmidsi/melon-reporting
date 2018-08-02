c04475d896bec121ecdcbce4cfb804ab98254a0b
Simon Emanuel Schmid <simon@schmid.io>
2018-08-02 14:24:36 +0200

    Rename fund simulator

--- src/api/reportDataGenerator.js
+++ src/api/reportDataGenerator.js
@@ -1,7 +1,7 @@
 import dataExtractor from './dataExtractor';
 
 import doFinalCalculations from '~/api/calculations/doFinalCalculations';
-import eventSourcingMocker from './mocking/eventSourcingMocker';
+import fundSimulator from './mocking/fundSimulator';
 
 import {
   mockStaticData,
@@ -21,7 +21,7 @@ const reportDataGenerator = async (
   if (fundAddress === '0xbada55') {
     // mock everything
     const emptyFund = await mockRandomEmptyFund();
-    const mockData = eventSourcingMocker(emptyFund);
+    const mockData = fundSimulator(emptyFund);
 
     const withFinalCalculations = doFinalCalculations(mockData);
 
