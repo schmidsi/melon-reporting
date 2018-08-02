868ce1b20bb20f4e5b95b031a5fbf4e3db8fd1f7
Simon Emanuel Schmid <simon@schmid.io>
2018-08-02 14:50:56 +0200

    Add invests/redeems

--- src/components/templates/Participations/index.jsx
+++ src/components/templates/Participations/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import * as R from 'ramda';
+import { format } from 'date-fns';
 
-import { format } from '~/utils/functionalBigNumber';
+import { format as numberFormat } from '~/utils/functionalBigNumber';
 import { Heading1 } from '~/components/design/typography';
 import { Container } from '~/components/design/layout';
 import Table from '~/components/blocks/Table';
 import HexValue from '~/components/blocks/HexValue';
 
-const columnConfig = {
+const investorsConfig = {
   address: {
     renderer: HexValue,
   },
@@ -36,13 +37,22 @@ const columnConfig = {
   },
 };
 
+const participationsConfig = {
+  token: { sortable: true },
+  type: { sortable: true },
+  value: { sortable: true },
+  shares: { sortable: true },
+  investor: { sortable: true, renderer: HexValue },
+  timestamp: { sortable: true },
+};
+
 const prepareInvestorTable = ({ data, calculations }) =>
   R.zipWith(R.merge, data.participations.investors, calculations.investors).map(
     investor => ({
       ...investor,
       kyc: 'unknown',
-      shares: format(investor.shares),
-      value: format(investor.value),
+      shares: numberFormat(investor.shares),
+      value: numberFormat(investor.value),
     }),
   );
 
@@ -52,8 +62,25 @@ const Participations = ({ data, calculations }) => (
       <Heading1>Participations</Heading1>
     </Container>
     <Container>
-      <Table columnConfig={columnConfig}>
+      <Table columnConfig={investorsConfig}>
         {prepareInvestorTable({ data, calculations })}
+      </Table>
+    </Container>
+    <Container>
+      <Heading1>Invests/Redeems</Heading1>
+    </Container>
+    <Container>
+      <Table columnConfig={participationsConfig}>
+        {data.participations.list.map(participation => ({
+          ...participation,
+          shares: numberFormat(participation.shares),
+          value: numberFormat(participation.value),
+          token: participation.token.symbol,
+          timestamp: format(
+            new Date(participation.timestamp * 1000),
+            'YYYY-MM-DD HH:mm:ss',
+          ),
+        }))}
       </Table>
     </Container>
   </div>
