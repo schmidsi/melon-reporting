e778fba106eeac75895092e034183b560a53994b
Simon Emanuel Schmid <simon@schmid.io>
2018-08-04 09:18:31 +0200

    Refactor a bit

--- src/components/blocks/AuditChart/index.jsx
+++ src/components/blocks/AuditChart/index.jsx
@@ -18,12 +18,51 @@ const Tick = ({ anchor, orientation, xScale, y, lineHeight, date }) => (
     />
     <text
       x={xScale(date)}
-      y={orientation === 'top' ? y + lineHeight : y - lineHeight}
+      y={orientation === 'top' ? y - lineHeight : y + lineHeight}
       textAnchor={anchor}
       className={styles.text}
     >
       {format(date, 'D. MMM YYYY')}
     </text>
+  </g>
+);
+
+const Xaxis = ({ start, middle, end, xScale, y, lineHeight }) => (
+  <g>
+    <line
+      x1={xScale(start)}
+      y1={y}
+      x2={xScale(end)}
+      y2={y}
+      className={styles.line}
+    />
+
+    <Tick
+      anchor="start"
+      orientation="top"
+      date={xScale.invert(0)}
+      xScale={xScale}
+      y={y}
+      lineHeight={lineHeight}
+    />
+
+    <Tick
+      anchor="middle"
+      orientation="top"
+      date={middle}
+      xScale={xScale}
+      y={y}
+      lineHeight={lineHeight}
+    />
+
+    <Tick
+      anchor="end"
+      orientation="top"
+      date={end}
+      xScale={xScale}
+      y={y}
+      lineHeight={lineHeight}
+    />
   </g>
 );
 
@@ -48,63 +87,7 @@ const AuditChart = () => {
 
   return (
     <svg width={width} className={styles.AuditChart}>
-      {/* Top axis */}
-      <line
-        x1={xScale(start)}
-        y1={yScale(2)}
-        x2={xScale(end)}
-        y2={yScale(2)}
-        className={styles.line}
-      />
-
-      <g>
-        <line
-          x1={xScale(start)}
-          y1={yScale(2)}
-          x2={xScale(start)}
-          y2={yScale(1.5)}
-          className={styles.line}
-        />
-        <text
-          x={xScale(start)}
-          y={yScale(1)}
-          textAnchor="start"
-          className={styles.text}
-        >
-          {format(xScale.invert(0), 'D. MMM YYYY')}
-        </text>
-      </g>
-
-      <g>
-        <line
-          x1={Math.round(xScale(middle))}
-          y1={2 * lineHeight}
-          x2={Math.round(xScale(middle))}
-          y2={1.5 * lineHeight}
-          className={styles.line}
-        />
-        <text
-          x={Math.round(xScale(middle))}
-          y={lineHeight}
-          textAnchor="middle"
-          className={styles.text}
-        >
-          {format(middle, 'D. MMM YYYY')}
-        </text>
-      </g>
-
-      <g>
-        <line
-          x1={width}
-          y1={2 * lineHeight}
-          x2={width}
-          y2={1.5 * lineHeight}
-          className={styles.line}
-        />
-        <text x={width} y={lineHeight} textAnchor="end" className={styles.text}>
-          {format(xScale.invert(width), 'D. MMM YYYY')}
-        </text>
-      </g>
+      <Xaxis {...{ start, middle, end, xScale, y: yScale(2), lineHeight }} />
 
       <g>
         <line
