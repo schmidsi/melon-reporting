2aadcb3e9bd70cb3c2bbcabcedb6971934d52cbb
Simon Emanuel Schmid <simon@schmid.io>
2018-08-04 18:12:18 +0200

    Add HoldingBars

--- src/components/blocks/HoldingBars/index.jsx
+++ src/components/blocks/HoldingBars/index.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { scaleLinear } from 'd3-scale';
+import { interpolateRound } from 'd3-interpolate';
 
-const HoldingBars = ({ allocations }) => {
+import styles from './styles.css';
+
+const HoldingBars = ({ allocations, children }) => {
   const lineHeight = Math.round((16 * 16) / 18);
   const width = Math.round((960 + 12) * (16 / 18));
-  const height = lineHeight * allocations.lenths;
+  const height = 2 * lineHeight * (allocations.length + 1);
 
-  return <svg width={width} />;
+  const xScale = scaleLinear()
+    .domain([0, 1])
+    .range([0, width])
+    .interpolate(interpolateRound);
+
+  const yScale = n => n * lineHeight;
+
+  return (
+    <div style={{ width, height }} className={styles.holder}>
+      <svg style={{ width, height }} className={styles.underlay}>
+        {allocations.map((holding, i) => (
+          <rect
+            x={0}
+            y={yScale(2 * i)}
+            height={yScale(1.5)}
+            width={xScale(holding.percentage)}
+            className={styles.holding}
+          />
+        ))}
+        <rect
+          x={0}
+          y={yScale(2 * allocations.length)}
+          height={yScale(1.5)}
+          width={xScale(1)}
+          className={styles.sum}
+        />
+      </svg>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 };
 
 export default HoldingBars;
--- src/components/blocks/HoldingBars/story.jsx
+++ src/components/blocks/HoldingBars/story.jsx
@@ -3,6 +3,8 @@ import { storiesOf } from '@storybook/react';
 
 import HoldingBars from '.';
 
+import styles from './story.css';
+
 const mockData = [
   { percentage: 0.5 },
   { percentage: 0.3 },
@@ -10,12 +12,12 @@ const mockData = [
 ];
 
 storiesOf('HoldingBars', module).add('default', () => (
-  <div>
+  <div className={styles.wrapper}>
     <HoldingBars allocations={mockData}>
-      <p>ASDF</p>
-      <p>QWER</p>
-      <p>YXCV</p>
-      <p>Fund</p>
+      <div>ASDF</div>
+      <div>QWER</div>
+      <div>YXCV</div>
+      <div>Fund</div>
     </HoldingBars>
   </div>
 ));
